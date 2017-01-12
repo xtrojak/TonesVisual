3efdf83a468a72507feddf52b5611ef8f5eef0d1
mathooo <trojak@mail.muni.cz>
2017-01-12 22:18:48 +0100

    Code refinement

--- paint.py
+++ paint.py
@@ -1,68 +1,88 @@
 import svgwrite
 import sys
 
+# definitions
+
 def myLine(image, fromPoint, toPoint, color):
     image.add(image.line(fromPoint, toPoint, stroke=color))
+
+def paintVerticalLine(image, size, i):
+    myLine(image, (50 + (i+1)*80, 50), (50 + (i+1)*80, size), "black")
+
+def paintHorizontalLine(image, i):
+    myLine(image, (50, 50 + (i+1)*50), (1010, 50 + (i+1)*50), "black")
 
 def myText(image, possition, text, color):
     image.add(image.text(text, insert=possition, fill=color))
 
-isGuitar = sys.argv[-2]
+def paintTone(image, i, j, tone):
+    myText(img, (85 + i*80, 30 + (j + 1)*50), tone, "red")
+
+def writeBarNumber(image, i):
+    myText(image, (80 + i*80, 30), str(i+1), "black")
+
+def writeBaseTone(image, i, tone, color):
+    myText(image, (30, 80 + i*50), tone, color)
+
+def writeTextUnderPicture(image, i, tones):
+    myText(image, (50, i), "tones: " + ", ".join(sorted(set(input_tones))), "black")
+
+def getViolinAttributes():
+    tones = [['f', 'f#', 'g', 'g#', 'a', 'a#', 'h', 'c', 'c#', 'd', 'd#', 'e'],
+             ['a#', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'],
+             ['d#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'h', 'c', 'c#', 'd'],
+             ['g#', 'a', 'a#', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g']]
+    default_tuning = ['g', 'd', 'a', 'e']
+    size = 0
+    return size, 4, tones, default_tuning
+
+def getGuitarAttributes():
+    tones = [['f', 'f#', 'g', 'g#', 'a', '#a', 'h', 'c', 'c#', 'd', 'd#', 'e'],
+             ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', '#a', 'h'],
+             ['g#', 'a', '#a', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g'],
+             ['d#', 'e', 'f', 'f#', 'g', 'g#', 'a', '#a', 'h', 'c', 'c#', 'd'],
+             ['#a', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'],
+             ['f', 'f#', 'g', 'g#', 'a', '#a', 'h', 'c', 'c#', 'd', 'd#', 'e']]
+    default_tuning = ['e', 'h', 'g', 'd', 'a', 'e']
+    size = 100
+    return size, 6, tones, default_tuning
+
+
+# handle input
+
+argument = sys.argv[-2]
 input_tones = sys.argv[-1]
 input_tones = input_tones.split(" ")
 
-tones_guitar = [['f', 'f#', 'g', 'g#', 'a', '#a', 'h', 'c', 'c#', 'd', 'd#', 'e'],
-                ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', '#a', 'h'],
-                ['g#', 'a', '#a', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g'],
-                ['d#', 'e', 'f', 'f#', 'g', 'g#', 'a', '#a', 'h', 'c', 'c#', 'd'],
-                ['#a', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'],
-                ['f', 'f#', 'g', 'g#', 'a', '#a', 'h', 'c', 'c#', 'd', 'd#', 'e']]
+# set attributes according to the choosen tool
 
-tones_violin = [['f', 'f#', 'g', 'g#', 'a', 'a#', 'h', 'c', 'c#', 'd', 'd#', 'e'],
-                ['a#', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a'],
-                ['d#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'h', 'c', 'c#', 'd'],
-                ['g#', 'a', 'a#', 'h', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g']]
+if argument == '-v': #if guitar is choosen
+    size, iterate, tones, tuning = getViolinAttributes()
+else:
+    size, iterate, tones, tuning = getGuitarAttributes()
 
-default_guitar_tuning = ['e', 'h', 'g', 'd', 'a', 'e']
-default_violin_tuning = ['g', 'd', 'a', 'e']
-tuning = default_violin_tuning
-tones = tones_violin
-
-xsize = 300
-iterate = 4
-line = 250
-text = 280
-if isGuitar == '-g': #if guitar is choosen
-    xsize = 400
-    iterate = 6
-    line = 350
-    text = 380
-    tuning = default_guitar_tuning
-    tones = tones_guitar
-
-img = svgwrite.Drawing(filename='result.svg', size=(1060, xsize), debug=True)
+img = svgwrite.Drawing(filename='result.svg', size=(1060, 300 + size), debug=True)
 img.add(img.rect(insert=(0, 0), size=('100%', '100%'), rx=None, ry=None, fill='rgb(255,255,255)'))
 
 color = "black"
 
-# paint vertical lines
-myLine(img, (50, 50), (50, line), color)
+paintVerticalLine(img, 250 + size, -1)
 for i in range(12):
-    myLine(img, (50 + (i+1)*80, 50), (50 + (i+1)*80, line), color)
-    myText(img, (80 + i*80, 30), str(i+1), color)
+    paintVerticalLine(img, 250 + size, i)
+    writeBarNumber(img, i)
 
-myLine(img, (50, 50), (1010, 50), color) # paint horizontal line
+paintHorizontalLine(img, -1)
 for string in range(iterate):
     color = "black"
-    myLine(img, (50, 50 + (string+1)*50), (1010, 50 + (string+1)*50), color)  # paint horizontal lines
+    paintHorizontalLine(img, string)
     if tuning[string] in input_tones:
         color = "red"
-    myText(img, (30, 80 + string*50), tuning[string], color)  # paint main tones
+    writeBaseTone(img, string, tuning[string], color)
 
     for bar in range(12):
         color = "red"
         if tones[string][bar] in input_tones:
-            myText(img, (85 + bar*80, 30 + (string + 1)*50), tones[string][bar], color)  # paint other tones
+            paintTone(img, bar, string, tones[string][bar])
 
-myText(img, (50, text), "tones: " + ", ".join(sorted(input_tones)), "black")    # write text under picture
+writeTextUnderPicture(img, 280 + size, input_tones)
 img.save()
